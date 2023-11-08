8a3dd84f8e48b4172d164c6b5c83fbd43f2c16a2
Nicholas Roberts <niccolox@devekko.com>
2023-11-07 20:55:16 -0800

    description

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Ai.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
